254c70d78d8e0eb81c2408786f8c6d52ee61a355
nsladkii <nsladkii@voximplant.com>
2021-04-17 10:58:44 +0300

    Add main gateway

---
+++ src/app.gateway.ts
@@ -0,0 +1,33 @@
+import { Logger } from '@nestjs/common';
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WsResponse,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway()
+export class AppGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
+  private logger: Logger = new Logger('AppGateway');
+
+  afterInit(server: Server) {
+    this.logger.log('The server has been initialized');
+  }
+
+  handleDisconnect(client: Socket) {
+    this.logger.log(`Client disconnected: ${client.id}`);
+  }
+
+  handleConnection(client: Socket, ...args: any[]) {
+    this.logger.log(`Client connected: ${client.id}`);
+  }
+
+  @SubscribeMessage('msgToServer')
+  handleMessage(client: Socket, message: string): WsResponse<string> {
+    return { event: 'msgToClient', data: message };
+  }
+}
